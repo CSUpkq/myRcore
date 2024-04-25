0558e57e3a1da267871c9b1eb927a45268c94b71
CSUpkq <2646416989@qq.com>
2024-04-25 09:01:08 +0800

    ch1

---
+++ os/src/syscall/fs.rs
@@ -0,0 +1,45 @@
+//! File and filesystem-related syscalls
+
+// const FD_STDOUT: usize = 1;
+
+// /// write buf of length `len`  to a file with `fd`
+// pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+//     match fd {
+//         FD_STDOUT => {
+//             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
+//             let str = core::str::from_utf8(slice).unwrap();
+//             print!("{}", str);
+//             len as isize
+//         }
+//         _ => {
+//             panic!("Unsupported fd in sys_write!");
+//         }
+//     }
+// }
+const FD_STDOUT: usize = 1;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
+use crate::batch::{USER_STACK};
+const USER_STACK_SIZE: usize = 4096;
+
+
+pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+    match fd {
+        FD_STDOUT => {
+            // unsafe {println!("#{:#x} {:#x} #", buf as usize , USER_STACK.get_sp() - USER_STACK_SIZE);}
+            if (((buf as usize)  >= USER_STACK.get_sp() - USER_STACK_SIZE) && ((buf as usize) + len <= USER_STACK.get_sp())) 
+            || (((buf as usize) + len <= APP_SIZE_LIMIT + APP_BASE_ADDRESS) && ((buf as usize) >= APP_BASE_ADDRESS)){
+                let slice = unsafe { core::slice::from_raw_parts(buf, len) };
+                let str = core::str::from_utf8(slice).unwrap();
+                print!("{}", str);
+                len as isize
+            }else{
+                -1 as isize
+            }
+        },
+        _ => {
+            -1 as isize 
+            // panic!("Unsupported fd in sys_write!");
+        }
+    }
+}
---
+++ user/src/bin/bad_address.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+#[macro_use]
+extern crate user_lib;
+
+/// 由于 rustsbi 的问题，该程序无法正确退出
+/// > rustsbi 0.2.0-alpha.1 已经修复，可以正常退出
+
+#[no_mangle]
+pub fn main() -> isize {
+    
+    unsafe {
+        println!("here!!!!");
+        #[allow(clippy::zero_ptr)]
+        (0x0 as *mut u8).write_volatile(0);
+    }
+    panic!("FAIL: T.T\n");
+}
